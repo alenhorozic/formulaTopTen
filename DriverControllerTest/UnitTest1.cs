bd2f86b270381498d86058960bc88f6c99faeb21
alenhorozic <alenhorozicrhotmail.com>
2021-01-29 12:48:41 +0100

    fix auto createdAt

---
+++ DriverControllerTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+
+namespace DriverControllerTest
+{
+    private DriverController controller;
+
+
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
